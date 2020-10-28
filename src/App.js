bdb5599088c667235b68d4b461dafbbd8a86c914
guilhermebehsgarupa <guilherme.behs@garupa.co>
2020-10-28 11:27:52 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, Component } from "react";
+import socketIOClient from "socket.io-client";
 import './App.css';
 
-function App() {
+
+
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      mensagens:[],
+      texto:'',
+      socket: socketIOClient('localhost:3001')
+    }
+
+    this.state.socket.on("broadcast", data => {
+      this.setState({mensagens:data})
+    });
+               
+    this.state.socket.on("atualizarMensagens", data => {
+      this.setState({mensagens:data})
+    });
+  }
+
+
+  onButtonClick(socket){
+  
+    socket.emit('enviarMensagem',this.state.texto);
+  };
+
+  gerarConversa(){
+
+    const conversa = this.state.mensagens.map((mensagem)=>
+       <div>
+          {mensagem.usuario}<br/>
+          {mensagem.dataHora}  -  {mensagem.texto} 
+          <br/>
+          <br/>
+      </div>
+         
+    )
+
+    return conversa;
+  }
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <input type="text"  onChange={e=>{this.setState({texto:e.target.value})}}/>
+        <button  onClick={()=>this.onButtonClick(this.state.socket)}>Enviar Mensagem</button>
+        <br/>
+        <br/>
+        
+        {this.gerarConversa()}
+
     </div>
   );
+  }
 }
 
 export default App;
